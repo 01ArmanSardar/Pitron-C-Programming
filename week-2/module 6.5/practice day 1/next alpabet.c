8a8b407667b7571595d17455afbaf882175efe24
ArmanSardar <mdarmansardar57@gmail.com>
2023-04-11 22:19:39 +0600

    practice

--- week-2/module 6.5/practice day 1/next alpabet.c
+++ week-2/module 6.5/practice day 1/next alpabet.c
@@ -2,5 +2,20 @@
 int main()
 {
     char ch;
+    scanf("%c",&ch);
+    if (ch >=97 && ch <= 121)
+    {
 
+        int x = ch +1;
+        printf("%c",x);
+
+    }
+    else  if (ch== 122)
+    {
+
+        printf("%c",97);
+    }
 }
+
+
+
