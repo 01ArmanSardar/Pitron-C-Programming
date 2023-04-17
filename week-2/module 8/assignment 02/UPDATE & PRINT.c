a902e4a6d4858cc6ba8fa3038f2718f1cfb2fb76
ArmanSardar <mdarmansardar57@gmail.com>
2023-04-17 12:03:04 +0600

    practice

--- week-2/module 8/assignment 02/UPDATE & PRINT.c
+++ week-2/module 8/assignment 02/UPDATE & PRINT.c
@@ -3,19 +3,20 @@ int main()
 {
     int n,x,v;
     scanf("%d",&n);
-   // int A[x];
-    //scanf("%d ",&A[x]);
-   // scanf("%d",&v);
     int A[n];
-   // scanf("%d ",&A[x]);
-//scanf("%d",&v);
     for (int i=0; i<n; i++)
     {
         scanf("%d\n",&A[i]);
-
     }
-     scanf("%d",&A[x]);
-scanf("%d",&v);
+    scanf("%d",&x);
+    scanf("%d",&v);
+    for (int i=0; i<n; i++)
+    {
+        if (i== x)
+        {
+            A[x]=v;
+        }
+    }
     for (int i=n-1; i>=0; i--)
     {
         printf("%d ",A[i]);
