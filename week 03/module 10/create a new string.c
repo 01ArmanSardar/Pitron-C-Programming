89c0db7d407f6347beac4b9ab983b965df1c4c11
ArmanSardar <mdarmansardar57@gmail.com>
2023-04-22 21:21:51 +0600

    pitron pracice

---
+++ week 03/module 10/create a new string.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    scanf("%d",&ar[n]);
+    for (int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    int br[m];
+    scanf("%d",&br[m]);
+    for (int i=0;i<m;i++)
+    {
+        scanf("%d",&br[i]);
+    }
+
+}
