09635229bd60da008f8e343f1a7ed1c52fc3efc8
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-01 13:10:08 +0600

    push practice

--- week 03/Module -11/string Lexicographical Comparison.c
+++ week 03/Module -11/string Lexicographical Comparison.c
@@ -2,37 +2,37 @@
 int main ()
 {
     char a[100],b[100];
-    scanf("%s %s",&a,&b);
-    int i=0;
+    scanf("%s %s ",&a,&b);
+    int i =0;
     while (1)
     {
         if (a[i]=='\0' && b[i]=='\0')
         {
-            printf("same");
+            printf ("same\n ");
             break;
         }
-        else if (a[i]='\0')
+        else if (a[i]=='\0')
         {
-            printf("a choto");
+            printf("a choto\n");
             break;
         }
-        else if (b[i]='\0');
+        else if (b[i]=='\0');
         {
-            printf("b choto ");
-            break;
+            printf ("b choto\n ");
+            break ;
         }
-        if (a[i]=b[i])
+        if (a[i]==b[i])
         {
             i++;
         }
         else if (a[i]<b[i])
         {
-            printf("a coto ");
-            break;
+            printf ("a coto\n");
+            break ;
         }
         else
         {
-            printf("b coto");
+            printf ("b coto\n");
             break;
         }
     }
