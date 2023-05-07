8bca3658fbf25d4c8741fd291507e8b3b285da35
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-07 23:01:21 +0600

    pitron practice

--- week 03/module 12 # Mid term exam/Count -1.c
+++ week 03/module 12 # Mid term exam/Count -1.c
@@ -1,24 +1,21 @@
-#include<stdio.h>
-int main ()
+#include <stdio.h>
+int main()
 {
     int n;
     scanf("%d",&n);
-    int ar [n];
-    int evan=0,odd=0,i;
-    for ( i=0;i<n;i++)
+    int ar[n];
+    int evan=0,odd=0;
+    for (int i=0;i<n;i++)
     {
         scanf("%d",&ar[i]);
+        if (ar[i]%2==0)
+        {
+            evan++;
+        }
+        else
+        {
+            odd++;
+        }
     }
-    if (i%2==0)
-    {
-        evan++;
-    }
-    else
-    {
-        odd++;
-    }
-    for (i=0;i<n;i++)
-    {
-        printf("%d %d",evan,odd);
-    }
+    printf("%d %d",evan,odd);
 }
