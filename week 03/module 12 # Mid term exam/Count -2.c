15bff6343007965b74e9ae522dcca56e139a68b1
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-09 11:17:38 +0600

    pitron practice

--- week 03/module 12 # Mid term exam/Count -2.c
+++ week 03/module 12 # Mid term exam/Count -2.c
@@ -1,13 +1,20 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 int main()
 {
-    int stri[]="armansardar";
-    int a,e,i,o,u,vowel=0;
-    for (int b=0;b<strlen;b++)
+    char  stri[50] = "abefjimotulodi";
+    // scanf("%s",&stri);
+   // char a,e,i,o,u;
+    int vowel=0;
+    for (int b=1; b<=strlen(stri);b++)
     {
-        if (stri[b]==a ||stri[b]==e || stri[b]==i || stri[b]==o || stri[b]==u)
+        if (stri[b]== 'a' || stri[b]== 'e' || stri[b]== 'i' || stri[b]== 'o' || stri[b]== 'u' )
+        {
             vowel ++;
+        }
+
     }
     printf("%d",vowel);
 }
