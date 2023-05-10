cc74f4eaf6abb035b35c9401a5ea71169c4411d8
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-10 06:14:03 +0600

    pitron practice

---
+++ week 03/module 12 # Mid term exam/Count -3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%s",s);
+    int cnt [26]={0};
+    for (int i=0;i<strlen(s);i++)
+    {
+        int value=s[i]-97;
+        cnt [value]++;
+    }
+    for (int i=0;i<26;i++)
+    {
+        printf("%c  - %d\n",i+97,cnt[i]);
+    }
+}
--- week 03/module 12 # Mid term exam/Find It.c
+++ week 03/module 12 # Mid term exam/Find It.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
 
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    int x;
+    int count =0;
+    scanf("%d",&x);
+    for (int i=0;i<=n;i++)
+    {
+        scanf("%d",&ar[i]);
+        if (ar[i]==x)
+            count++;
+    }
+    printf("%d",count);
+}
