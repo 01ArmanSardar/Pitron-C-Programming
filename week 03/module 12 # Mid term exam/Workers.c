ece816e1f7c2939eb3ef6ecf8e4b42d53c586b57
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-12 00:59:40 +0600

    pitron practice

--- week 03/module 12 # Mid term exam/Workers.c
+++ week 03/module 12 # Mid term exam/Workers.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+    int a,b,c,result;
+    scanf("%d %d %d",&a ,&b,&c);
+    result =(a*c)/b;
+    printf("%d",result);
+}
