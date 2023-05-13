cb9f240d83614644f591b8ccba129d59f7291003
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-13 17:34:02 +0600

    pitron practice

---
+++ Week 04/Module -14/Function 1(Returrn + Parameter)difrent way.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*function syntex
+Return_type    Name     (Parameter)
+{
+    Code
+    Return What?
+}*/
+int sum (int x,int y)
+{
+   int su=x+y;
+    return su;
+}
+int main()
+{
+    printf("%d\n",sum(10,20));
+        printf("%d",sum(10,200));
+
+}
+
