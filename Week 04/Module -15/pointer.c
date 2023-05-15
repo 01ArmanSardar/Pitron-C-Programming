f172d78a68ba69494e84653f1ac89e4ea0cb7f2d
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-15 19:37:10 +0600

    pitorn practice

--- Week 04/Module -15/pointer.c
+++ Week 04/Module -15/pointer.c
@@ -3,6 +3,6 @@ int main()
 {
     int x=10;
     int *pjr =&x;
-    *pjr =90;
-printf("%d",*pjr);
+    *pjr =90;//derefarance kora
+printf("%d",*pjr);//derefarence korle x er value keh acces kora jai
 }
