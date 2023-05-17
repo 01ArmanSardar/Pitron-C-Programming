9794c66a116bb9e84fb3f29258c5dc704791c2b4
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-17 23:12:01 +0600

    pitron practice

---
+++ week 05 - 2d Array/MOdule -18 (2d array )/2D Array input output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //declare 2d array
+    int ar [5][4];
+    for (int i=0;i<5;i++)
+    {
+        for (int j=0;j<4;j++)
+        {
+            printf("a[%d] [%d]",i,j);
+        }
+        printf("\n");
+    }
+}
