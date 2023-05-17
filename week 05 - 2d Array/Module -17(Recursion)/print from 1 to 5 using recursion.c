86ddd1769458e918962a59111540e4bf9c577bef
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-17 15:56:15 +0600

    pitron practice

--- week 05 - 2d Array/Module -17(Recursion)/print from 1 to 5 using recursion.c
+++ week 05 - 2d Array/Module -17(Recursion)/print from 1 to 5 using recursion.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void fun (int i)
 {
-    if (i==6)return;
+    if (i==6)return; // a line tah holo base case ,akhnei amra bolhe dibho jeh recursion tah kotokhon projnto cholbeh , Amra jodhi a base case er maddomeh aktah limit set nah koreh dei tahole Recursion tah infinity choltei takbhe
    printf("%d\n",i);
    fun(i+1);
 }
