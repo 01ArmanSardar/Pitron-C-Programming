15e0dce1fb70c88230106ae1b6619b03e9fc47ce
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-18 13:56:12 +0600

    pitron practice

--- week 05 - 2d Array/MOdule -18 (2d array )/zero matrix or Null matrix.c
+++ week 05 - 2d Array/MOdule -18 (2d array )/zero matrix or Null matrix.c
@@ -1,35 +1,34 @@
 #include <stdio.h>
 int main()
 {
-    int row ,col;
-    scanf("%d %d ",&row ,&col);
+    int row,col;
+    scanf("%d %d ",&row,&col);
     int ar[row][col];
     //int element =row*col;
-    for (int i=0;i<row;i++)
+    for (int i=0; i<row; i++)
     {
-        for (int i=0;i<col;i++)
+        for (int j=0; j<col; i++)
         {
-            scanf("%d",&ar[row][col]);
+            scanf("%d",&ar[i][j]);
         }
     }
     int count=0;
-    for (int i=0;i<row;i++)
+    for (int i=0; i<row; i++)
     {
-        for (int i=0;i<col;i++)
+        for (int j=0; j<col; j++)
         {
-            if (ar[row][col]!=0)
+            if (ar[i][j]!=0)
             {
                 count++;
             }
         }
-
     }
-    if (count==0)
-        {
-            printf("this is nulll matrix\n");
-        }
-        else
-        {
-            printf("matrix is not null\n");
-        }
+    if (count=0)
+    {
+        printf("this is nulll matrix\n");
+    }
+    else
+    {
+        printf("matrix is not null\n");
+    }
 }
