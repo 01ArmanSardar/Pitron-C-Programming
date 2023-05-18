078eb5a963f165656818cdf9eb956e1d4ed0ce9e
ArmanSardar <mdarmansardar57@gmail.com>
2023-05-18 10:58:54 +0600

    pitron practice

--- week 05 - 2d Array/MOdule -18 (2d array )/2D Array Printing by Row & Column.c
+++ week 05 - 2d Array/MOdule -18 (2d array )/2D Array Printing by Row & Column.c
@@ -1,11 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    int row ,col;
+    int row,col;
     scanf("%d %d ",&row,&col);
     int ar[row][col];
-    for(int i=0;i<row;i++)
+    for(int i=0; i<row; i++)
     {
-
+        for (int j=0; j<col; j++)
+        {
+scanf("%d",&ar[i][j]);
+        }
     }
+    //exact column
+  /*  int e;
+    scanf("%d",&e);
+    for (int i=0;i<row;i++)
+    {
+        printf("%d ",ar[i][e]);
+    }*/
+    int e;
+    scanf("%d",&e);
+    for (int i=0;i<col;i++)
+    {
+        printf("%d ",ar[e][i]);
+    }
+
+
 }
